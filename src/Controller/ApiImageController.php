f3272978bfa13ede6720dabeab85d8ed3719f8b7
Pavel <officialwebpage@yandex.ru>
2019-05-22 23:44:22 +0300

    Added classes and libs.

---
+++ src/Controller/ApiImageController.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class ApiImageController
+{
+//
+
+
+    public function storeFile()
+    {
+
+    }
+
+    public function saveFileFromUrl()
+    {
+
+    }
+
+    public function saveFileFromBase64()
+    {
+
+    }
+
+    public function createResize()
+    {
+
+    }
+
+    public function getImageResizes()
+    {
+
+    }
+
+    public function deleteImageResize()
+    {
+
+    }
+
+    public function deleteAllImageResizes()
+    {
+
+    }
+}
---
+++ src/Entity/Image.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\ImageRepository")
+ */
+class Image
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $original_name;
+
+    /**
+     * @ORM\Column(type="json", nullable=true)
+     */
+    private $file_info = [];
+
+    /**
+     * @ORM\Column(type="json", nullable=true)
+     */
+    private $resizes = [];
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updated_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getOriginalName(): ?string
+    {
+        return $this->original_name;
+    }
+
+    public function setOriginalName(string $original_name): self
+    {
+        $this->original_name = $original_name;
+
+        return $this;
+    }
+
+    public function getFileInfo(): ?array
+    {
+        return $this->file_info;
+    }
+
+    public function setFileInfo(?array $file_info): self
+    {
+        $this->file_info = $file_info;
+
+        return $this;
+    }
+
+    public function getResizes(): ?array
+    {
+        return $this->resizes;
+    }
+
+    public function setResizes(?array $resizes): self
+    {
+        $this->resizes = $resizes;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
